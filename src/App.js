25ced90a22c6071925e0b6e3a5a445af1ec8ec88
RehanMehde <71442362+RehanMehde@users.noreply.github.com>
2020-09-20 03:29:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Wiht Rehan Mehdi
+          Learn React With Rehan Mehdi
         </a>
       </header>
     </div>
